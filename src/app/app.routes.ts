0e162bc525f4d36b5b3a7038a0267e97b97f19e6
SokmarK <hleontorres05@gmail.com>
2024-09-13 03:44:14 -0500

    Cambios v2 13-09-2024

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,7 +1,6 @@
 import { Routes } from '@angular/router';
 import { PrincipalComponent } from './components/principal/principal.component';
 import { ProductsComponent } from './components/products/products.component';
-import { GuideComponent } from './components/guide/guide.component';
 import { ContactComponent } from './components/contact/contact.component';
 import { ClientsComponent } from './components/clients/clients.component';
 import { AboutComponent } from './components/about/about.component';
@@ -12,7 +11,6 @@ import { ReferencesComponent } from './components/references/references.componen
 export const routes: Routes = [
     { path: "", component: PrincipalComponent },
     { path: "products", component: ProductsComponent },
-    { path: "guide", component: GuideComponent },
     { path: "contact", component: ContactComponent },
     { path: "clients", component: ClientsComponent },
     { path: "about", component: AboutComponent },
